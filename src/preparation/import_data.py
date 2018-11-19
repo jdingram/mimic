ac253bbccf1371f0fac02c3ffb8551f9ab77d410
James Ingram <James@MacBook-Pro-3.local>
2018-11-19 11:44:29 +0000

    setup complete

---
+++ src/preparation/import_data.py
@@ -0,0 +1,41 @@
+'''
+
+This module is for importing the data
+
+'''
+
+import pandas as pd
+import psycopg2 as p
+
+
+def get_table(host, dbname, schema, table, columns, where='row_id>0'):
+
+	'''
+	Function for importing the Mimic data from a Postgres database.
+
+	'''
+
+	# Select DB location
+	con = p.connect("host={} dbname={}".format(host, dbname))
+	cur = con.cursor()
+
+	# Execute query
+	cur.execute("SELECT {} FROM {}.{} WHERE {}".format(columns, schema, table, where))
+	print('Query extraction executed')
+
+	if cur.rowcount > 0:
+		# Get rows and column names
+		rows=cur.fetchall()
+		column_names = [desc[0] for desc in cur.description]
+
+		# Create DataFrame
+		df = pd.DataFrame(rows)
+		df.columns = column_names
+
+		# De-Dupe
+		df.drop_duplicates(inplace=True)
+
+		print('DF shape is: ' + str(df.shape))
+		return df
+	else:
+		print('No rows returned')
